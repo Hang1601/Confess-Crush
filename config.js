a90ef0b0297f9e7441ddd7ad014d940ab174d1e3
Hang1601 <97169717+Hang1601@users.noreply.github.com>
2022-01-05 20:18:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Anh ơi!Em có điều muốn nói",
+    titleWeb: "Anh ơi!Em có điều muốn nói"
     introTitle: 'Trần Văn Hiếu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đấy là thương`,
+    introDesc: `Trái đất vốn lạ thường,
+    Mà sao anh cứ đi nhầm đường.
+    Lạc vào tim em lẻ loi,
+    Đằng sau chữ yêu đây là thương`.
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Yêu lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu mình em <3',
+    btnReply: 'gửi anh trái tym của em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Anh có yêu em không.',
-    btnAccept: 'có chứ <3',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Yêu anh nhất trên đời.',
+    btnAccept: 'Yêu anh 300 <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
